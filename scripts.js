c0a2f06897fce4ce43f5a6211bb5ce03b1d01f3d
Dobi Okrasa <dobi.okrasa@gmail.com>
2017-12-22 20:50:58 +0100

    Fix scripts & add particles.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-  $.getJSON(quoteUrl, createTweet);
+  $.getJSON(prefix + quoteUrl, createTweet);
+  $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
@@ -30,6 +32,118 @@ function createTweet(input) {
 $(document).ready(function() {
   getQuote();
   $('.trigger').click(function() {
-      getQuote();
+    getQuote();
   })
+});
+
+
+particlesJS("particles-js", {
+	"particles": {
+		"number": {
+			"value": 256,
+			"density": {
+				"enable": true,
+				"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#ffffff"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#000000"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": 0.5,
+			"random": false,
+			"anim": {
+				"enable": false,
+				"speed": 1,
+				"opacity_min": 0.1,
+				"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+				"enable": false,
+				"speed": 40,
+				"size_min": 0.1,
+				"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 5,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "bounce",
+			"bounce": false,
+			"attract": {
+				"enable": true,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "repulse"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 400,
+				"line_linked": {
+					"opacity": 1
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 200,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
 });
